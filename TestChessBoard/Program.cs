87b59b186b69a5ffbc74b8d103efc5a767bfbde1
vagnani <vagnani01@gmail.com>
2016-03-06 21:26:56 +0100

    bugfix

--- TestChessBoard/Program.cs
+++ TestChessBoard/Program.cs
@@ -29,6 +29,7 @@ namespace TestChessBoard
             var locked = Adding.CreateLockedCells(Console.ReadLine());
 
             Console.WriteLine("Inserire direzioni consentite: seguendo la rosa dei venti");
+            Console.WriteLine("Direzioni possibili:(nne)(ene)(ese)(sse) o (nno)(ono)(oso)(sso)");
             Console.WriteLine("Usare seguente formalismo: (...) <== parentesi obbligatorie");
             var directions = Adding.CreateDirections(Console.ReadLine());
 
@@ -41,16 +42,69 @@ namespace TestChessBoard
                 start, arrive, limit, locked, directions
                 );
 
-            foreach (var list in chess)
+            do
             {
-                //my implementation
-                string toprint = "";
-                foreach (var node in list)
+                Console.WriteLine("passante per qualche nodo?");
+                string str = Console.ReadLine().Trim();
+
+                if (str != "no")
                 {
-                    toprint += node.ToString();
+                    List<List<Coordinate>> nodeFiltered = new List<List<Coordinate>>();
+                    Console.WriteLine("Inserire coordinate da passare");
+                    Console.WriteLine("Usare seguente formalismo: (x,y) <== parentesi obbligatorie");
+                    var compulsaryNode = Adding.CreateLockedCells(Console.ReadLine());
+
+                    foreach (var list in chess)
+                    {
+                        bool listAdded = false;
+                        foreach (var element in list)
+                        {
+                            foreach (var MustNode in compulsaryNode)
+                            {
+                                if (element.Equals(MustNode))
+                                {
+                                    nodeFiltered.Add(list); listAdded = true; ; break;
+                                }
+                            }
+
+                            if(listAdded)
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                    foreach (var list in nodeFiltered)
+                    {
+                        string toprint = "";
+                        foreach (var node in list)
+                        {
+                            toprint += node.ToString();
+                        }
+                        Console.WriteLine(toprint);
+                    }
                 }
-                Console.WriteLine(toprint);
-            }
+
+                else
+                {
+                    foreach (var list in chess)
+                    {
+                        string toprint = "";
+                        foreach (var node in list)
+                        {
+                            toprint += node.ToString();
+                        }
+                        Console.WriteLine(toprint);
+                    }
+                }
+
+                Console.WriteLine("Vuoi continuare?");
+                string toContinue = Console.ReadLine();
+                if (toContinue == "no")
+                {
+                    break;
+                }
+            } while (true);
 
         Console.ReadLine();
         }
